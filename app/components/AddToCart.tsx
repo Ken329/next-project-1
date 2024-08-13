f0d22acd47276f97713fcaf581ac2a757d193d98
Ken Liau <67051443+Ken329@users.noreply.github.com>
2024-08-13 17:30:47 +0800

    [init] initial commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button
+        className="btn btn-primary"
+        onClick={(e) => console.log("Hello World")}
+      >
+        Add to cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
